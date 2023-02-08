ca4249712d1958b9fb0e5cbbfbedf507386c9b12
Joseph Ellis <josephellis@Josephs-MacBook-Air.local>
2023-02-08 14:35:58 -0600

    Created register page

---
+++ src/components/register.js
@@ -0,0 +1,21 @@
+import "./register.css"
+
+export default function Register() {
+    return (
+        <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input className="registerInput" type="text" placeholder="Enter your username..." />
+        <label>Email</label>
+        <input className="registerInput" type="text" placeholder="Enter your email..." />
+        <label>Password</label>
+        <input className="registerInput" type="password" placeholder="Enter your password..." />
+        <label>Location</label>
+        <input className="registerInput" type="text" placeholder="Enter your location..." />
+        <button className="registerButton">Register</button>
+      </form>
+        <button className="registerLoginButton">Login</button>
+    </div>
+    )
+}
