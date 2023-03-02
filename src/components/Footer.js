597cd164d68cd097a932f72f6adb2ab2b4c4f08a
Joseph Ellis <josephellis@Josephs-MacBook-Air.local>
2023-03-02 13:16:31 -0600

    lowercased the letters in the profile page and commeted out ajax

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Footer.css'
-
 function Footer() {
-
-    $.ajax({
+  /*  $.ajax({
         url: "https://api.adviceslip.com/advice",
-      
         dataType: "json",
         success: function(result){
             console.log(result)
-    
             let advice = document.getElementById("advice");
                 advice.innerHTML = result.slip.advice;
-            
         }
     })
-
+*/
     return (
         <>
             <div
@@ -26,10 +21,8 @@ function Footer() {
                     justifyContent: 'center',
                     marginTop: '5rem',
                 }}>
-
                 <a className="navbar-brand align-items-center" href="/">
                     <img src={process.env.PUBLIC_URL + '/Images/upperlip8.png'} height="200" alt="Upper Lip Holstery" /></a>
-                
             </div>
             <div className="text-center pb-0">
                 <br></br>
@@ -37,19 +30,15 @@ function Footer() {
             </div>
             <div id="advice" class="row justify-content-evenly">
             </div>
-
             {/* <div className="text-center pb-0">
                 <br></br>
                 <p className='everything'>Everything that you do, do it as for the Lord</p>
             </div> */}
-
             <footer id="footer" class="text-center mt-5">
                 <div className="logoBottom">
                     <a className="navbar-brand align-items-center" href="/">
                         <img src={process.env.PUBLIC_URL + '/Images/upperlip10.png'} height="100" alt="Upper Lip Holstery" /></a>
-                    
                 </div>
-
                 <div className="text-center pb-3 text-light ">
                     <p className="p" >© 2023 Status Undefined  .  Web Development:</p>
                     <p className="p2" >Austin J Fogle . Joseph Ellis . Josiah Buell . Nanette Guzman . Timothy Roberts . Yissel L Nunez</p>
@@ -58,5 +47,4 @@ function Footer() {
         </>
     )
 }
-
 export default Footer
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,23 +2,16 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Outlet, Link } from 'react-router-dom'
 import '../styles/Profile.css'
-
 import { Stack, Button, Row, Col, Container, Card } from 'react-bootstrap';
 import SignUp from './SignUp';
-
 import UserContext from '../context/UserContext';
 import Login from './Login'
 // import ProfileCoverImage from './ProfileCoverImage';
 import EditProfile from './EditProfile';
 // import { GiScissors, GiOfficeChair } from 'react-icons/gi';
 // import { MdPhoneIphone } from 'react-icons/md';
-
-
-
 function Profile() {
-
     let { getBarberById } = useContext(UserContext);
-
     let params = useParams()
     const [oneBarber, setOneBarber] = useState({
         barberId: params.barberId,
@@ -32,11 +25,8 @@ function Profile() {
         profilePic: "",
         description: "",
     });
-
     console.log(params.barberId);
-
     let { barberId, firstName, lastName, address, city, state, phoneNumber, licenseNumber, profilePic, description } = oneBarber
-
     useEffect(() => {
         if (barberId === undefined) return
         async function fetch() {
@@ -46,7 +36,6 @@ function Profile() {
         }
         fetch()
       }, [barberId])
-
     return (
         <>
             <div className="editButtonPosition">
@@ -54,27 +43,24 @@ function Profile() {
                     <Button id='editButton' onClick={<EditProfile />}  > EDIT PROFILE</Button>
                 </Link>
             </div>
-
             <section>
                 <div className='bgCoverProfile'>
                     <div className="container" style={{ minHeight: '600px' }}>
                         <div className="text-center justify-content-center align-self-center">
                             <div className="h1" >
-                                <img id="pic" src={ProfilePic} class="rounded-circle" height="150" alt="" />
-                                <h1>{FirstName} {LastName}</h1>
+                                <img id="pic" src={profilePic} class="rounded-circle" height="150" alt="" />
+                                <h1>{firstName} {lastName}</h1>
                             </div>
-
                             <div className="explore" >
-                                <p>License: {LicenseNumber}</p>
+                                <p>License: {licenseNumber}</p>
                                 <br /><br />
-                                <p> {Description}
+                                <p> {description}
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-
             {/* Promotion targeting Barbers */}
             <section>
                 <div className='barberTitle'><h1>Contact Me</h1></div>
@@ -83,19 +69,16 @@ function Profile() {
                         <Card>
                             <Row>
                                 <Col className='div1'>
-                                    <img src={ProfilePic} height="600" alt="" />
+                                    <img src={profilePic} height="600" alt="" />
                                 </Col>
                                 <Col className='div2'>
                                     <br></br>
-                                    <h3 id='membership'>{FirstName} {LastName}</h3>
+                                    <h3 id='membership'>{firstName} {lastName}</h3>
                                     <br></br>
-
                                      {/* <h3 id='membership'> -------------<GiScissors />-------------</h3> */}
-
                                     <p>* List your business</p>
                                     <p>* Let new client find you</p>
                                     <p>* We promote you in external platforms</p>
-
                                     <Link to="/SignUp" className="nav-link m-0">
                                         <Button id='signUpButton' onClick={<SignUp />} className="mt-4 mb-4 ly-0" > SIGN UP</Button>
                                     </Link>
@@ -105,7 +88,6 @@ function Profile() {
                         </div>
                     </div>
                 </section>
-
                 {/* Promotion targeting barber's clients */}
                 <section>
                     <div className='barberTitle'><h1>Looking for a Professional Barber?</h1></div>
@@ -115,11 +97,9 @@ function Profile() {
                             <Row>
                                 <Col className='div3'>
                                     <p>Our certified and skilled barbers are ready to provide you with an amazing uplifting experience; using the latest technology, high quality products, and creating the finest styles, our professionals are ready to serve you! </p>
-
-
                                     <br></br>
-                                    <p> (Call or text) {PhoneNumber}</p>
-                                    <p> {Address}, {City}, {State}</p> */}
+                                    <p> (Call or text) {phoneNumber}</p>
+                                    <p> {address}, {city}, {state}</p>
                                     <br></br>
                                     <h3 id='membership'> -----------------------------</h3>
                                 </Col>
@@ -128,7 +108,6 @@ function Profile() {
                     </div>
                 </div>
             </section>
-
             {/* Promotion targeting barber's clients */}
             <section>
                 <div className='barberTitle'><h1>Gallery</h1></div>
@@ -137,14 +116,12 @@ function Profile() {
                         <Row>
                             <Col className='div3'>
                                 <p>Our certified and skilled barbers are ready to provide you with an amazing uplifting experience; using the latest technology, high quality products, and creating the finest styles, our professionals are ready to serve you! </p>
-
                                 <br></br>
                                 <h3 id='membership'>Start looking awesome</h3>
                                 <br></br>
                                 <p>* Skilled professionals</p>
                                 <p>* Certified</p>
                                 <p>* Top Products</p>
-
                                 <Link to="/BarberList" className="nav-link m-0">
                                     <Button id='signUpButton' onClick className="mt-4 mb-4 ly-0" > FIND A BARBER</Button>
                                 </Link>
@@ -156,7 +133,6 @@ function Profile() {
                     </div>
                 </div>
             </section>
-
             {/* Gallery Section */}
             <section>
                 <div className='bgGalleryCover'>
@@ -166,11 +142,9 @@ function Profile() {
                                 <h1>Trending Now...</h1>
                                 <h1>The Upper Lip Holstery</h1>
                             </div>
-
                             <div className="verse" >
                                 <p>To these four young men God gave knowledge and understanding of all kinds of literature and learning.</p>
                             </div>
-
                             <Link to="/Gallery" className="nav-link m-2">
                                 <Button id='signInButton' className="mt-4 mb-4 ly-0" >LEARN MORE</Button>
                             </Link>
@@ -179,17 +153,13 @@ function Profile() {
                     </div>
                 </div>
             </section>
-
             {/* <ProfileCoverImage /> */}
-
             <Stack>
                 <Outlet />
             </Stack>
             {/* ) */}
         </>
-
         // </UserContext.Consumer>
     )
 }
-
 export default Profile
